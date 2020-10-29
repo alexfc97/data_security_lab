3a72bd7d853f6b21ef2d2db9681043d4b21fd052
Alexander <alexfc@live.dk>
2020-10-29 12:34:57 +0100

    Big Update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Data Security Lab
+
+To run the program please repeat the following:
+- First run the Server main method
+- Then run the Client main method
+
+Once the client method is run you will now have the option of running the different methods available to a client.
--- src/main/java/dtu/dk/client/Client.java
+++ src/main/java/dtu/dk/client/Client.java
@@ -7,6 +7,8 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+
 public class Client {
 
     public static void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException, FileNotFoundException {
@@ -27,5 +29,20 @@ public class Client {
 
         // Try invalid token
         System.out.println(service.queue("invalid token","Printer1"));
+
+        ArrayList<String> optionsList = new ArrayList<String>();
+        optionsList.add("Print");
+        optionsList.add("Queue");
+        optionsList.add("TopQueue");
+        optionsList.add("start");
+        optionsList.add("stop");
+        optionsList.add("restart");
+        optionsList.add("status");
+        optionsList.add("readConfig");
+        optionsList.add("setConfig");
+        while(true){
+            for (String option: optionsList)
+                System.out.format("%d. %s",optionsList.indexOf(option),option);
+        }
     }
 }
